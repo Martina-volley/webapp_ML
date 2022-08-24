a0887269662d98f3cebd1c813cc04734c88cc1d6
MartinaChen <volleycogit.chen@gmail.com>
2022-08-24 15:20:36 +0800

    fix image path

--- web_app.py
+++ web_app.py
@@ -11,10 +11,10 @@ st.title('Beach volleyball analytic app')
 
 # Multicolumn Support
 col1, col2 = st.columns(2)
-frame1 = Image.open(r"/images/annotated_frame_1.png")
+frame1 = Image.open("annotated_frame_1.png")
 col1.header("Time_1")
 col1.image(frame1, use_column_width=True)
-frame2 = Image.open(r"/images/annotated_frame_2.png")
+frame2 = Image.open("annotated_frame_2.png")
 col2.header("Time_2")
 col2.image(frame2, use_column_width=True)
 
